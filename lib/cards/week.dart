5093f95ed863e289293fd25e44f1e63e91beb96d
Gary Pendergast <gary@pento.net>
2020-01-03 22:12:43 +1000

    Add styling to the Week Card (#14)

--- lib/cards/week.dart
+++ lib/cards/week.dart
@@ -24,12 +24,40 @@ class _WeekCardState extends State<WeekCard> {
             itemBuilder: ( BuildContext context, int index ) {
               WeatherForecastTemperature day = weather.week.days[ index ].forecast.temperature;
               return Container(
-                height: 24,
+                margin: EdgeInsets.symmetric( vertical: 0, horizontal: 12 ),
+                padding: EdgeInsets.symmetric( vertical: 6, horizontal: 0 ),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: index == weather.week.days.length - 1 ? BorderSide( width: 0 ) : BorderSide( color: Color.fromARGB( 0xFF, 0xCC, 0xCC, 0xCC )),
+                  ),
+                ),
                 child: Row(
                   children: <Widget>[
-                    Text( index == 0 ? 'Tomorrow' : Jiffy( weather.week.days[ index ].dateTime ).format( 'EEEE' ) ),
+                    Expanded(
+                        child: Text( index == 0 ? 'Tomorrow' : Jiffy( weather.week.days[ index ].dateTime ).format( 'EEEE' ) )
+                    ),
                     Icon( weatherIcon( day.code ) ),
-                    Text( '${ day.min } ${ day.max }' )
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '    ${ day.min.toString() }    ',
+                            style: TextStyle(
+                              color: Colors.blue,
+                            ),
+                          ),
+                          TextSpan(
+                            text: day.max.toString(),
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               );
