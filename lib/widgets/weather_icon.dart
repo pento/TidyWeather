4b88e8fa0d5af109dbf26cb954fbb9615f585f5c
Gary Pendergast <gary@pento.net>
2020-01-05 21:48:03 +1000

    Add all of the possible weather icons (#26)

--- lib/widgets/weather_icon.dart
+++ lib/widgets/weather_icon.dart
@@ -3,14 +3,36 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 
 IconData weatherIcon( String iconCode ) {
   Map iconCodes = new Map();
+
+  iconCodes[ 'chance-shower-cloud' ] = MdiIcons.weatherRainy;
   iconCodes[ 'chance-shower-fine' ] = MdiIcons.weatherPartlyRainy;
+  iconCodes[ 'chance-snow-cloud' ] = MdiIcons.weatherSnowy;
+  iconCodes[ 'chance-snow-fine' ] = MdiIcons.weatherPartlySnowy;
+  iconCodes[ 'chance-thunderstorm-cloud' ] = MdiIcons.weatherLightning;
   iconCodes[ 'chance-thunderstorm-fine' ] = MdiIcons.weatherPartlyLightning;
+  iconCodes[ 'chance-thunderstorm-showers' ] = MdiIcons.weatherLightningRainy;
   iconCodes[ 'cloudy' ] = MdiIcons.weatherCloudy;
+  iconCodes[ 'drizzle' ] = MdiIcons.weatherRainy;
   iconCodes[ 'dust' ] = MdiIcons.weatherHazy;
+  iconCodes[ 'few-showers' ] = MdiIcons.weatherRainy;
   iconCodes[ 'fine' ] = MdiIcons.weatherSunny;
+  iconCodes[ 'fog' ] = MdiIcons.weatherFog;
+  iconCodes[ 'frost' ] = MdiIcons.snowflakeVariant;
+  iconCodes[ 'hail' ] = MdiIcons.weatherHail;
+  iconCodes[ 'heavy-showers-rain' ] = MdiIcons.weatherPouring;
+  iconCodes[ 'heavt-snow' ] = MdiIcons.weatherSnowyHeavy;
+  iconCodes[ 'high-cloud' ] = MdiIcons.weatherPartlyCloudy;
+  iconCodes[ 'light-snow' ] = MdiIcons.weatherSnowy;
+  iconCodes[ 'mostly-cloudy' ] = MdiIcons.weatherCloudy;
   iconCodes[ 'mostly-fine' ] = MdiIcons.weatherPartlyCloudy;
+  iconCodes[ 'overcast' ] = MdiIcons.weatherCloudy;
   iconCodes[ 'partly-cloudy' ] = MdiIcons.weatherPartlyCloudy;
   iconCodes[ 'shower-or-two' ] = MdiIcons.weatherPartlyRainy;
+  iconCodes[ 'showers-rain' ] = MdiIcons.weatherRainy;
+  iconCodes[ 'snow' ] = MdiIcons.weatherSnowy;
+  iconCodes[ 'snow-and-rain' ] = MdiIcons.weatherSnowyRainy;
+  iconCodes[ 'thunderstorm' ] = MdiIcons.weatherLightning;
+  iconCodes[ 'wind' ] = MdiIcons.weatherWindy;
 
   if ( iconCodes.containsKey( iconCode ) ) {
     return iconCodes[ iconCode ];
