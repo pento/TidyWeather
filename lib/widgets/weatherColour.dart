e369e9c86f3012c528aae792cea8bd9ca05d5ad6
Gary <gary@pento.net>
2020-06-14 23:18:43 +1000

    Tidy up some deprecated theme data names.

--- lib/widgets/weatherColour.dart
+++ lib/widgets/weatherColour.dart
@@ -8,18 +8,18 @@ Color weatherColor( BuildContext context, String weatherCode ) {
 
   Map weatherCodes = new Map();
 
-  weatherCodes[ 'chance-shower-cloud' ] = Colors.blueGrey.shade400;
-  weatherCodes[ 'chance-shower-fine' ] = Colors.green.shade500;
+  weatherCodes[ 'chance-shower-cloud' ] = Colors.blueGrey;
+  weatherCodes[ 'chance-shower-fine' ] = Colors.green;
 //  weatherCodes[ 'chance-snow-cloud' ] = 'weather_snowy';
 //  weatherCodes[ 'chance-snow-fine' ] = 'weather_partly_snowy';
 //  weatherCodes[ 'chance-thunderstorm-cloud' ] = 'weather_lightning';
 //  weatherCodes[ 'chance-thunderstorm-fine' ] = 'weather_partly_lightning';
 //  weatherCodes[ 'chance-thunderstorm-showers' ] = 'weather_lightning_rainy';
 //  weatherCodes[ 'cloudy' ] = 'weather_cloudy';
-  weatherCodes[ 'drizzle' ] = Colors.blueGrey.shade300;
+  weatherCodes[ 'drizzle' ] = Colors.blueGrey;
 //  weatherCodes[ 'dust' ] = 'weather_hazy';
-  weatherCodes[ 'few-showers' ] = Colors.blueGrey.shade400;
-//  weatherCodes[ 'fine' ] = 'weather_fine';
+  weatherCodes[ 'few-showers' ] = Colors.blueGrey;
+  weatherCodes[ 'fine' ] = Colors.lightBlue;
 //  weatherCodes[ 'fog' ] = 'weather_fog';
 //  weatherCodes[ 'frost' ] = 'weather_frost';
 //  weatherCodes[ 'hail' ] = 'weather_hail';
@@ -28,11 +28,11 @@ Color weatherColor( BuildContext context, String weatherCode ) {
 //  weatherCodes[ 'high-cloud' ] = 'weather_partly_cloudy';
 //  weatherCodes[ 'light-snow' ] = 'weather_snowy';
 //  weatherCodes[ 'mostly-cloudy' ] = 'weather_cloudy';
-  weatherCodes[ 'mostly-fine' ] = Colors.green.shade500;
+  weatherCodes[ 'mostly-fine' ] = Colors.green;
 //  weatherCodes[ 'overcast' ] = 'weather_cloudy';
   weatherCodes[ 'partly-cloudy' ] = Colors.lightBlue;
-  weatherCodes[ 'shower-or-two' ] = Colors.green.shade500;
-  weatherCodes[ 'showers-rain' ] = Colors.blueGrey.shade400;
+  weatherCodes[ 'shower-or-two' ] = Colors.green;
+  weatherCodes[ 'showers-rain' ] = Colors.blueGrey;
 //  weatherCodes[ 'snow' ] = 'weather_snowy';
 //  weatherCodes[ 'snow-and-rain' ] = 'weather_snowy_rainy';
 //  weatherCodes[ 'thunderstorm' ] = 'weather_lightning';
